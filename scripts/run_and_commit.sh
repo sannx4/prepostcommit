0342fe38e1e3d96e2afe60b9c5c1981e61c3ba4e
sanjay <sanjay26120002@snuchennai.edu.in>
2026-07-22 12:14:04 +0530

    add automatic report commit

---
+++ scripts/run_and_commit.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+PROJECT_DIR="$HOME/automation-lab"
+
+cd "$PROJECT_DIR"
+
+echo "running system report"
+
+./scripts/sysreport.sh
+
+echo "addding log files to git"
+
+git add logs/
+
+if git diff --cached --quiet; then
+echo "no new log changes to commit"
+else
+git commit -m "automatic system report $(date '+%F %T')"
+echo "log commited"
+fi
+
+echo "automation done"
+
+
